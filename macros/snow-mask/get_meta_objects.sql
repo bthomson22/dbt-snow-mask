7bfb319188ab1e6b63087b590e981e8837b8713b
Jeremy Cohen <jeremy@dbtlabs.com>
2025-06-07 03:35:34 +0200

    More defensive code

--- macros/snow-mask/get_meta_objects.sql
+++ macros/snow-mask/get_meta_objects.sql
@@ -10,28 +10,34 @@
         
         {% if meta_key is not none %}
             {% if node_resource_type == "source" %} 
-                {% for column in columns if meta_key in graph.sources[node_unique_id]['columns'][column]['config']['meta'] %}
-                    {% set meta_dict = graph.sources[node_unique_id]['columns'][column]['config']['meta'] %}
-                    {% set policy_name = meta_dict[meta_key] %}
-                    {% if "mp_conditional_columns" in meta_dict %}
-                        {% set conditional_columns = meta_dict['mp_conditional_columns'] %}
-                    {% else %}
-                        {% set conditional_columns = [] %}
+                {% for column in columns %}
+                    {% set col = graph.sources[node_unique_id]['columns'][column] %}
+                    {% if col['config'] and meta_key in col['config']['meta'] %}
+                        {% set meta_dict = col['config']['meta'] %}
+                        {% set policy_name = meta_dict[meta_key] %}
+                        {% if "mp_conditional_columns" in meta_dict %}
+                            {% set conditional_columns = meta_dict['mp_conditional_columns'] %}
+                        {% else %}
+                            {% set conditional_columns = [] %}
+                        {% endif %}
+                        {% set meta_tuple = (column, policy_name, conditional_columns) %}
+                        {% do meta_columns.append(meta_tuple) %}
                     {% endif %}
-                    {% set meta_tuple = (column, policy_name, conditional_columns) %}
-                    {% do meta_columns.append(meta_tuple) %}
                 {% endfor %}
             {% else %}
-                {% for column in columns if meta_key in graph.nodes[node_unique_id]['columns'][column]['config']['meta'] %}
-                    {% set meta_dict = graph.nodes[node_unique_id]['columns'][column]['config']['meta'] %}
-                    {% set policy_name = meta_dict[meta_key] %}
-                    {% if "mp_conditional_columns" in meta_dict %}
-                        {% set conditional_columns = meta_dict['mp_conditional_columns'] %}
-                    {% else %}
-                        {% set conditional_columns = [] %}
+                {% for column in columns %}
+                    {% set col = graph.nodes[node_unique_id]['columns'][column] %}
+                    {% if col['config'] and meta_key in col['config']['meta'] %}
+                        {% set meta_dict = col['config']['meta'] %}
+                        {% set policy_name = meta_dict[meta_key] %}
+                        {% if "mp_conditional_columns" in meta_dict %}
+                            {% set conditional_columns = meta_dict['mp_conditional_columns'] %}
+                        {% else %}
+                            {% set conditional_columns = [] %}
+                        {% endif %}
+                        {% set meta_tuple = (column, policy_name, conditional_columns) %}
+                        {% do meta_columns.append(meta_tuple) %}
                     {% endif %}
-                    {% set meta_tuple = (column, policy_name, conditional_columns) %}
-                    {% do meta_columns.append(meta_tuple) %}
                 {% endfor %}
             {% endif %}
         {% else %}
