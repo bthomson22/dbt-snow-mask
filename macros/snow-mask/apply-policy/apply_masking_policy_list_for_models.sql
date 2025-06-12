5534f6ae62a30c371027013b335d2dacf0acf50d
bthomson22 <brandon.thomson@dbtlabs.com>
2025-06-11 18:12:33 -0600

    Reverted back to old resource type method

--- macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
+++ macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
@@ -8,9 +8,10 @@
         {% set alias    = model.alias %}
         {% set database = model.database %}
         {% set schema   = model.schema %}
+        {% set model_resource_type = model.resource_type | string %}
         
         {# -- TODO: Implement model.resource_type in post-hook context #}
-        {% set model_resource_type = model.unique_id.split('.')[0] %}
+        {# {% set model_resource_type = model.unique_id.split('.')[0] %} #}
 
         {% if model_resource_type|lower in ["model", "snapshot"] %}
 
